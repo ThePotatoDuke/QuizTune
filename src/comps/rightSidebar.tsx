00903f2a2c9d058f1d775216a0e8ef0766a2f0a3
HNHoly <96684788+HNHoly@users.noreply.github.com>
2024-12-10 22:26:09 +0300

    misc changes

--- src/comps/rightSidebar.tsx
+++ src/comps/rightSidebar.tsx
@@ -1,6 +1,5 @@
 /* This side panel is suitable for a music player provided by Spotify's API */
 import React from "react";
-import styled from "styled-components";
 import Sidebar from "./Sidebar";
 
 const RightSidebar: React.FC<{ children?: React.ReactNode }> = ({ children }) => {
