bff02f4ae61c6c256d513a7fd6b7819afd9cedb0
HNHoly <96684788+HNHoly@users.noreply.github.com>
2024-12-24 10:26:23 +0300

    hmmm

--- quiz_frontend/src/pages/History.tsx
+++ quiz_frontend/src/pages/History.tsx
@@ -11,7 +11,6 @@ import StatsSidebar from "../comps/StatsSidebar"; // Correct case
 interface Quiz {
     id: number;
     name: string;
-    // Add other fields as necessary
 }
 
 const Layout = styled.div`
